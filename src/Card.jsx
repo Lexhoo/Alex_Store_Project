1993a2dd2fccea4f6d0142b7543b755926cb3450
Alex Dogoum <lexhoo@yahoo.fr>
2019-06-06 21:30:01 +0200

    first commit

---
+++ src/Card.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Card extends Component {
+    render() {
+        return (
+            <div>
+              <h3>Hi from Card</h3>  
+            </div>
+        )
+    }
+}
